0f321a73ad70337d4c930bc8f5149b9649c7991a
FrankLef <flefebvre01@hotmail.com>
2022-02-06 06:49:05 -0500

    chap 3

---
+++ lib/gt_measures.R
@@ -0,0 +1,54 @@
+#' Create a table of effect measures with their CI
+#' 
+#' Create a table of effect measures with their CI.
+#' 
+#' The table will have 3 columns. The measure name, the estimates and the 
+#' confidence interval.
+#'
+#' @param df Dataframe of results
+#' @param conf Numeric, confidence interval
+#' @param digits Integer, number of digits to the right of the decimal.
+#' @param title The title of the table
+#' @param subtitle Subtitle of the table
+#'
+#' @return A gt object create by the `gt` package
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' }
+gt_measures <- function(df, conf = df$conf[1], digits = 3, 
+                        title = "Title", subtitle = "Subtitle") {
+  # confidence interval label used in footnote
+  ci_label <- sprintf("%.0f%% confidence interval", 100 * conf)
+  
+  df %>%
+    select(name, est, lci, uci) %>%
+    mutate(est = round(est, digits),
+           lci = round(lci, digits),
+           uci = round(uci, digits),
+           ci = paste0("(", lci, ", ", uci, ")"),
+           lci = NULL,
+           uci = NULL) %>%
+    gt() %>%
+    tab_header(
+      title = md(paste0("**", title, "**")),
+      subtitle = md(paste0("**", subtitle, "**"))
+    ) %>%
+    cols_align(align = "center", columns = !matches("name")) %>% 
+    cols_label(
+      name = "Measure",
+      est = "Estimate",
+      ci = "CI") %>%
+    opt_row_striping() %>%
+    tab_footnote(
+      footnote = ci_label,
+      locations = cells_column_labels(columns = ci)
+      ) %>%
+    tab_source_note(
+      source_note = "Fundamentals of Causal Inference, Babette A. Brumback, 2022"
+      ) %>%
+    tab_options(
+      heading.background.color = "gainsboro",
+      column_labels.font.weight = "bold")
+}
