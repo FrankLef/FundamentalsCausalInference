88cc95101a5f39aed54d47de89c38ea2cffe9192
FrankLef <flefebvre01@hotmail.com>
2022-02-11 08:29:36 -0500

    chap 1

--- lib/fci_01-B_mortability.R
+++ lib/fci_01-B_mortability.R
@@ -14,10 +14,7 @@ data_mortability <- function() {
     "T" = c(TRUE, TRUE, FALSE, FALSE),
     "H" = c(FALSE, TRUE, FALSE, TRUE),
     "deaths" = c(756340, 2152660, 2923480, 7517520),
-    "population" = c(282305227, 48262955, 1297258493, 133015479),
-    "Y" = c(0.002679, 0.044603, 0.002254, 0.056516)
-  )
-  stopifnot(dplyr::near(sum(out$Y), sum(out$deaths / out$population),
-                        tol = 1e-6))
+    "population" = c(282305227, 48262955, 1297258493, 133015479))
+  out$Y <- out$deaths / out$population
   out
 }
