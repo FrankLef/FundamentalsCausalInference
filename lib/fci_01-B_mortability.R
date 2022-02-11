5aa775613903a984d97c18adfb0b4690f6eb8160
FrankLef <flefebvre01@hotmail.com>
2022-02-11 08:38:56 -0500

    chap 1

--- lib/fci_01-B_mortability.R
+++ lib/fci_01-B_mortability.R
@@ -16,5 +16,8 @@ data_mortability <- function() {
     "deaths" = c(756340, 2152660, 2923480, 7517520),
     "population" = c(282305227, 48262955, 1297258493, 133015479))
   out$Y <- out$deaths / out$population
+  # verify with book
+  check <- c(0.002679, 0.0446, 0.002254, 0.05652)
+  stopifnot(sum(abs(out$Y - check)) < 0.0001)
   out
 }
