8d6784fa24b5c49db3f311e67d1f65db1029772c
FrankLef <flefebvre01@hotmail.com>
2022-02-12 02:37:18 -0500

    section 6.2

--- lib/fci_01-A_mortability.R
+++ lib/fci_01-A_mortability.R
@@ -21,3 +21,63 @@ data_mortability <- function() {
   stopifnot(sum(abs(out$Y - check)) < 0.0001)
   out
 }
+
+#' Create Mortability by Country dataset for exposure modeling
+#' 
+#' Create Mortability by Country dataset for exposure modeling.
+#' 
+#' Create Mortability by Country dataset for exposure modeling as described
+#' in section 6.2 of chapter 6. The function also perform the ATT calculations
+#' used in section 6.2.1.
+#'
+#' @return Dataframe of mortability rates and exposure model
+#' @export
+#'
+#' @examples
+#' data_mortability_exp()
+data_mortability_exp <- function() {
+  out <- data.frame(
+    "H" = c(0, 0, 0, 0, 1, 1, 1, 1),
+    "T" = c(0, 0, 1, 1, 0, 0, 1, 1),
+    "Y" = c(0, 1, 0, 1, 0, 1, 0, 1),
+    "n" = c(1297258493 - 2923480,
+            2923480,
+            282305227 - 756340,
+            756340,
+            133015479 - 7517520,
+            7517520,
+            48262955 - 2152660,
+            2152660))
+  out$p <- out$n / sum(out$n)  # compute proportion who died
+  # compute e(H=0)
+  dat0 <- out[out$H == 0, ]
+  eH0 <- sum(dat0$n[dat0$`T` == 1]) / sum(dat0)
+  # compute e(H=1)
+  dat1 <- out[out$H == 1, ]
+  eH1 <- sum(dat1$n[dat1$`T` == 1]) / sum(dat1)
+  # compute e(H) for all participants
+  out$eH <- eH0 * (1 - out$H) + eH1 * out$H
+  # compute the summands of the estimating equations
+  out$s1 <- out$`T` * out$Y / out$eH
+  out$s0 <- (1 - out$`T`) * out$Y / (1 - out$eH)
+  
+  # estimate the expected values of the potential outcomes
+  EY1 <- sum(out$s1 * out$p)
+  EY0 <- sum(out$s0 * out$p)
+  
+  # include the ATT calculations
+  # estimate P(T = 1)
+  e0 <- sum(out$`T` * out$p)
+  # compute the summands of the estimating equation
+  s <- out$Y * (1 - out$`T`) * out$eH / (e0 * (1 - out$eH))
+  # estimate E(Y0|T=1)
+  EY0T1 <- sum(s * out$p)
+  
+  # test results
+  stopifnot(dplyr::near(EY0, 0.0078399, tol = 1e-7),
+            dplyr::near(EY1, 0.0069952, tol = 1e-7),
+            dplyr::near(EY0T1, 0.010176, tol = 1e-6))
+  
+  # final output
+  list("EY1" = EY1, "EY0" = EY0, "EY0T1" = EY0T1, "data" = out)
+}
